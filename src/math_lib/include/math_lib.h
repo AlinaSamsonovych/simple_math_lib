20c25998e4f6e1890583dc6b4eca1ef82cce8a51
AlinaSamsonovych <126179084+AlinaSamsonovych@users.noreply.github.com>
2025-09-18 15:23:00 +0300

    Update math_lib.h

--- src/math_lib/include/math_lib.h
+++ src/math_lib/include/math_lib.h
@@ -70,3 +70,9 @@ namespace MathLib
     // Function to search GCD
     int GCD(int a, int b);
 }
+int factorial(int n);
+int factorial(int n) {
+	if (n < 0) return -1;
+	if (n == 0 || n == 1) return 1;
+	return n * factorial(n - 1);
+}
