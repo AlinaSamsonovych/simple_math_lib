41290332b685d54de2568c134726077f6e01cf1b
AlinaSamsonovych <126179084+AlinaSamsonovych@users.noreply.github.com>
2025-09-18 15:28:50 +0300

    Update math_library_tests.cpp

--- tests/math_library_tests.cpp
+++ tests/math_library_tests.cpp
@@ -77,3 +77,9 @@ TEST_F(MathLibTestFixture, GreatestCommonDivider)
 {
     EXPECT_EQ(MathLib::GCD(10, 6), 2);
 }
+TEST(MathLibTest, Factorial) {
+	EXPECT_EQ(factorial(0), 1);
+	EXPECT_EQ(factorial(1), 1);
+	EXPECT_EQ(factorial(5), 120);
+	EXPECT_EQ(factorial(7), 5040);
+}
